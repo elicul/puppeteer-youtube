b9a04661700caa0fc55b6a4db864f1ec6a7ba10b
elicul <enzo.licul@yahoo.com>
2019-05-21 20:30:06 +0200

    Youtube crawler update

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
 const puppeteer = require('puppeteer')
+var fs  = require("fs")
 try {
   (async () => {
+    
+    var songs = fs.readFileSync('songs-name.txt').toString().split('\n')
     const headless = process.argv[3] === 'true';
     const browser = await puppeteer.launch({
         headless: headless
     })
     const page = await browser.newPage()
-    await page.goto('https://youtube.com',  {waitUntil: 'networkidle2'})
-    await page.type('#search', 'Fleetwood Mac Dreams')
-    await page.click('button#search-icon-legacy')
-    await page.waitForSelector('a#video-title')
-    const videos = await page.$$('#video-title')
-    await videos[0].click()
-    await page.waitForSelector('.html5-video-container')
-    console.log(page.url())
+    
+    for (let index = 0; index < songs.length; index++) {
+        const song = songs[index];
+        await page.goto('https://youtube.com',  {waitUntil: 'networkidle2'})
+        await page.type('#search', song)
+        await page.waitForSelector('button#search-icon-legacy')
+        await page.click('button#search-icon-legacy')
+        await page.waitForSelector('a#video-title')
+        const videos = await page.$$('#video-title')
+        await videos[0].click()
+        await page.waitForSelector('.html5-video-container')
+        fs.appendFileSync('songs-url.txt', `${page.url()}\n`);
+    }
     await browser.close()
   })()
 } catch (err) {

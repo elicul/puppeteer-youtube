25dd2c16fb5efc75d55b9ee2b9245ab02a4981f8
elicul <enzo.licul@yahoo.com>
2019-05-22 08:02:11 +0200

    Update

--- index.js
+++ index.js
@@ -8,19 +8,22 @@ try {
     const browser = await puppeteer.launch({
         headless: headless
     })
-    const page = await browser.newPage()
     
     for (let index = 0; index < songs.length; index++) {
+        const page = await browser.newPage()
         const song = songs[index];
         await page.goto('https://youtube.com',  {waitUntil: 'networkidle2'})
         await page.type('#search', song)
         await page.waitForSelector('button#search-icon-legacy')
         await page.click('button#search-icon-legacy')
-        await page.waitForSelector('a#video-title')
-        const videos = await page.$$('#video-title')
+        await page.waitForSelector('ytd-thumbnail.ytd-video-renderer')
+        const videos = await page.$$('ytd-thumbnail.ytd-video-renderer')
         await videos[0].click()
         await page.waitForSelector('.html5-video-container')
+        await page.waitFor(5000)
+        console.log(page.url())
         fs.appendFileSync('songs-url.txt', `${page.url()}\n`);
+        await page.close();
     }
     await browser.close()
   })()
